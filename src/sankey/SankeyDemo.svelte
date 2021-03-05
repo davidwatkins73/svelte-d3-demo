8151961d40a6a0962434c603298a1561c6fa95ab
David Watkins <davidwatkins73@gmail.com>
2021-03-05 14:50:49 +0000

    Added mini sankeys

--- src/sankey/SankeyDemo.svelte
+++ src/sankey/SankeyDemo.svelte
@@ -6,7 +6,8 @@
 
 </script>
 
-<div style="display: inline-block">
+<width style="display: inline-block; width: 65%">
+
     <Sankey data={mkData()}/>
     <Sankey data={mkData()}/>
     <Sankey data={mkData()}/>
@@ -26,12 +27,19 @@
     <Sankey data={mkData()}/>
     <Sankey data={mkData()}/>
 
+</width>
+
+<div style="display: inline-block; width: 30%; vertical-align: top">
+    <p>Experimenting with lots of tiny sankeys.</p>
+    <p>Use the checkbox below to configure how the links are coloured (by-source or by-target)</p>
+
     <label for="color-by">
         Colour by Source:
         <input id="color-by"
                type="checkbox"
                bind:checked={$colorBySource}>
     </label>
+
 </div>
 
 <style>
