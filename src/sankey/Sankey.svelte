196f5551a0e3b8f2cbc9abfe92866b01d33a8246
David Watkins <davidwatkins73@gmail.com>
2021-03-05 16:19:00 +0000

    Adding more interactivity to sankey demo

--- src/sankey/Sankey.svelte
+++ src/sankey/Sankey.svelte
@@ -43,19 +43,24 @@
     function drawNodes(layout, elem, color) {
         const nodes = elem
             .selectAll(".node")
-            .data(layout.nodes, d => d.name);
+            .data(layout.nodes, d => d.id);
 
         const newNodes = nodes
             .enter()
             .append("rect")
-            .classed("node", true);
-
-        nodes.merge(newNodes)
+            .classed("node", true)
             .attr("x", d => d.x0)
             .attr("y", d => d.y0)
             .attr("width", d => d.x1 - d.x0)
             .attr("height", d => d.y1 - d.y0)
             .style("fill", d => color(d.category.code));
+
+        nodes
+            .merge(newNodes)
+            .transition()
+            .duration(200)
+            .attr("y", d => d.y0)
+            .attr("height", d => d.y1 - d.y0);
 
         nodes.exit().remove();
     }
@@ -64,21 +69,25 @@
     function drawLinks(layout, linksElem, color, categoryProvider = d => d.source.category.code) {
         const links = linksElem
             .selectAll(".link")
-            .data(layout.links, d => d.source.name + "_" + d.target.name);
+            .data(layout.links, d => `${d.source.id}_${d.target.id}`);
 
         const newLinks = links
             .enter()
             .append("path")
-            .classed("link", true);
+            .classed("link", true)
+            .attr("d", d3.sankeyLinkHorizontal())
+            .style("stroke", d => color(categoryProvider(d)))
+            .style("stroke-width", d => Math.max(1, d.width))
+            .style("fill", "none")
+            .style("opacity", 0.6);
 
         links
             .merge(newLinks)
+            .transition()
+            .duration(200)
             .attr("d", d3.sankeyLinkHorizontal())
+            .style("stroke", d => color(categoryProvider(d)))
             .style("stroke-width", d => Math.max(1, d.width))
-            .style("fill", "none")
-            .style("opacity", 0.6)
-            .transition().duration(300)
-            .attr("stroke", d => color(categoryProvider(d)))
     }
 
 
@@ -115,8 +124,8 @@
 
 <div style="display: inline-block">
     <svg bind:this={svgEl}
-         width="50"
-         height="75"
+         width="40"
+         height="60"
          viewbox="0 0 {dimensions.width} {dimensions.height}">
         <g transform="translate({margins.left} {margins.top})"
            bind:this={chartEl}>
--- src/sankey/SankeyDemo.svelte
+++ src/sankey/SankeyDemo.svelte
@@ -1,33 +1,26 @@
 <script>
+    import _ from "lodash";
+
     import {mkData} from "./data.js"
     import {colorBySource} from "./config-store"
     import Sankey from "./Sankey.svelte";
+
+    let chartData = _.map(_.range(30), () => mkData());
+
 </script>
 
 
-<width style="display: inline-block; width: 65%">
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-    <Sankey data={mkData()}/>
-</width>
+<div class="charts" style="display: inline-block; width: 65%">
+    {#each chartData as d}
+    <span on:click={() => d = mkData()} class="clickable">
+        <Sankey data={d}/>
+    </span>
+    {/each}
+</div>
 
 <div class="info">
     <p>Experimenting with lots of tiny sankeys.</p>
+    <p>Click on a sankey chart to randomise the data.</p>
     <p>Use the checkbox below to configure how the links are coloured (by-source or by-target)</p>
 
     <label for="color-by">
@@ -36,9 +29,20 @@
                type="checkbox"
                bind:checked={$colorBySource}>
     </label>
+
+    <br>
+
+    <button on:click={() => chartData = [...chartData, mkData()]}>
+        Mwoar
+    </button>
 </div>
 
 <style>
+
+    .charts {
+        user-select: none;
+    }
+
     .info {
         display: inline-block;
         width: 30%;
