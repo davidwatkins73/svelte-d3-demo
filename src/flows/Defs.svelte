71990edb54cf0f9a8c921b00f37ee98ffd15a331
David Watkins <davidwatkins73@gmail.com>
2021-03-17 22:12:19 +0000

    tidying up

--- src/flows/Defs.svelte
+++ src/flows/Defs.svelte
@@ -1,10 +1,24 @@
 <defs>
-    <linearGradient spreadMethod="pad" id="gradient-in" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" style="stop-color:#91dbf1;stop-opacity:1;" />
-        <stop offset="100%" style="stop-color:#def9fa;stop-opacity:1;" />
+    <linearGradient spreadMethod="pad"
+                    id="gradient-in"
+                    x1="0%"
+                    y1="0%"
+                    x2="100%"
+                    y2="0%">
+        <stop offset="0%"
+              style="stop-color:#91dbf1;stop-opacity:1;" />
+        <stop offset="100%"
+              style="stop-color:#def9fa;stop-opacity:1;" />
     </linearGradient>
-    <linearGradient spreadMethod="pad" id="gradient-out" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" style="stop-color:#def9fa;stop-opacity:1;" />
-        <stop offset="100%" style="stop-color:#91dbf1;stop-opacity:1;" />
+    <linearGradient spreadMethod="pad"
+                    id="gradient-out"
+                    x1="0%"
+                    y1="0%"
+                    x2="100%"
+                    y2="0%">
+        <stop offset="0%"
+              style="stop-color:#def9fa;stop-opacity:1;" />
+        <stop offset="100%"
+              style="stop-color:#91dbf1;stop-opacity:1;" />
     </linearGradient>
 </defs>
